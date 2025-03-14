cbc430f81acedd824699323d4e95e7ccbeabaed0
Arman-2107046 <armanr.rafi@gmail.com>
2025-03-14 20:25:36 +0600

    delete products from cart

--- data/cart.js
+++ data/cart.js
@@ -1,35 +1,47 @@
-export const cart=[
-    {
-        productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity:4
-    },
-    {
-        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity:2
-    }
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 4,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
 ];
 
-
 //add to cart function
-export function addToCart(productId)
-{
- // Check if the product is already in the cart
- let matchingItem = null;
+export function addToCart(productId) {
+  // Check if the product is already in the cart
+  let matchingItem = null;
 
- cart.forEach((cartItem) => {
-   if (productId === cartItem.productId) {
-     matchingItem = cartItem;
-   }
- });
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
- // Increase quantity if the product is already in the cart
- if (matchingItem) {
-   matchingItem.quantity++;
- } else {
-   // Add the product to the cart if it's not already present
-   cart.push({
-     productId: productId,
-     quantity: 1,
-   });
- }
+  // Increase quantity if the product is already in the cart
+  if (matchingItem) {
+    matchingItem.quantity++;
+  } else {
+    // Add the product to the cart if it's not already present
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
+
+//removing
+
+export function removeFromCart(productId) {
+  let newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,11 @@
-import { cart } from "../data/cart.js";
+import { cart,removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+// import { removeFromCart } from "../data/cart.js";
+
+
+
+
 
 let cartSummaryHTML = ``;
 
@@ -35,7 +40,10 @@ cart.forEach((cartItem) => {
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">Update</span>
-            <span class="delete-quantity-link link-primary">Delete</span>
+            
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">Delete</span>
+
+
           </div>
         </div>
 
@@ -71,3 +79,18 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+
+link.addEventListener('click', ()=>{
+
+    // console.log("delete");
+
+    const productId=link.dataset.productId;
+removeFromCart(productId);
+console.log(cart);
+
+});
+
+});
